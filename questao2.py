5a91baca7cc45cd42008905f5e7fa5cd9d758c44
pedrohenriqueads <pedrosilva@ads.fiponline.edu.br>
2020-03-11 20:46:10 -0300

    Modificado

--- questao2.py
+++ questao2.py
@@ -6,4 +6,12 @@ minutos_banho = 5
 agua_banho = 45
 total_litros = 1000
 soma = total_litros / agua_banho
-print("%.f banhos" % soma)
+print("Em um banho de 5 minutos, 1m3 litros, permite que você tome %.f banhos" % soma)
+
+# acrescimo de questao
+
+tempo_banho = int(input("Diga quantos minutos você passa no banho: "))
+litros_minutos = 9
+gasto_no_banho = tempo_banho * litros_minutos
+quantidade_banho = 1000 / gasto_no_banho
+print("Para um banho de %d minutos, 1m3 permite %d banhos" % (tempo_banho, quantidade_banho))
--- questao4.py
+++ questao4.py
@@ -1,5 +1,12 @@
 #Questâo 4
 
-temp_f = int(input("Digite a temperatura em fahrenheit: "))
+temp_f = float(input("Digite a temperatura em fahrenheit: "))
 celsius = 5 * (temp_f - 32) / 9
-print("A temperatura em celsius é %d graus" % celsius)
+print("A temperatura em celsius é %.2f graus" % celsius)
+
+# convertendo para fahrenheit
+
+temp_c = float(input("Digite a temperatura em celsius: "))
+fahrenheit = temp_c * (9 / 5) + 32
+print("A temperatura em fahrenheit é %.2f graus" % fahrenheit)
+
