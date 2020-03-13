b0e369b05992dd36dd172aad250fff5f80b0faf8
pedrohenriqueads <pedrosilva@ads.fiponline.edu.br>
2020-03-13 19:40:19 -0300

    Modificado

--- questao7.py
+++ questao7.py
@@ -18,10 +18,10 @@ largura4 = float(input("Digite a lagura do quarto comodo: "))
 comp4 = float(input("Digite o comprimento do quarto comodo: "))
 r4 = largura4 * comp4
 
-print("O primeiro comodo mede %.2f ²" % r1)
-print("O segundo comodo mede %.2f cm²" % r2)
-print("O terceiro comodo mede %.2f cm²" % r3)
-print("O quarto comodo mede %.2f cm²" % r4)
+print("O primeiro comodo mede %.2f m²" % r1)
+print("O segundo comodo mede %.2f m²" % r2)
+print("O terceiro comodo mede %.2f m²" % r3)
+print("O quarto comodo mede %.2f m²" % r4)
 
 area_total = (largura1 + largura2 + largura3 + largura4) * (comp1 + comp2 + comp3 + comp4)
-print("A area total da casa é %.2f cm²" % area_total)
+print("A area total da casa é %.2f m²" % area_total)
