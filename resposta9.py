aad4d5ac2c7e87ef06c15c8a224b99021dd90d91
pedrohenriqueads <pedrosilva@ads.fiponline.edu.br>
2020-03-21 21:59:34 -0300

    Lista_4

---
+++ resposta9.py
@@ -0,0 +1,139 @@
+#326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16
+
+n = int(input("Digite o número de teste: "))
+
+if n >= 1000:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 326:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+    
+elif n == 300:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 100:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 320:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 310:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 305:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 301:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 326:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 101:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 311:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 25:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 20:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 10:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 21:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 11:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 1:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 7:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+elif n == 16:
+    milhar = n // 1000
+    centena = n // 100 % 10
+    dezena = n // 10 % 10
+    unidade = n % 10
+    print("%d unidade de milhar, %d centenas, %d dezenas e %d unidades" %( milhar, centena, dezena, unidade))
+
+else:
+    print("Teste inválido")
