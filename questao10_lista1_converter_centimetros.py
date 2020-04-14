93db81215f97e5e00765330679dfe5681dc70daa
pedrohenriqueads <pedrosilva@ads.fiponline.edu.br>
2020-04-14 18:54:57 -0300

    Lista_1

---
+++ questao10_lista1_converter_centimetros.py
@@ -0,0 +1,4 @@
+c = float(input("Digite o valor em centimetros: "))
+resultado = c / 2.54
+
+print("O valor convertido é %.2f " % resultado)
---
+++ questao6_lista1_converter_fahrenheit.py
@@ -0,0 +1,5 @@
+#6 - Escreva um programa que leia uma temperatura em graus Fahrenheit, transforme-a em graus Celsius e exiba o resultado.
+
+temp_f = float(input("Digite a temperatura em fahrenheit: "))
+celsius = 5 * (temp_f - 32) / 9
+print("A temperatura em celsius é %.2f graus" % celsius)
