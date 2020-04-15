036710d3113b098e68a8b7d3cf5816d7fc129906
pedrohenriqueads <pedrosilva@ads.fiponline.edu.br>
2020-04-15 20:05:50 -0300

    apagando_arquivos

--- questao_1.py
+++
@@ -1,8 +0,0 @@
-#1 - Faça um Programa que peça dois números e imprima o maior deles.
-
-n1 = int(input("Digite o primeiro número: "))
-n2 = int(input("Digite o segundo número: "))
-if n1 > n2:
-    print(n1)
-else:
-    print(n2)
--- questao_2.py
+++
@@ -1,8 +0,0 @@
-#2 - Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
-
-n = int(input("Digite o valor:"))
-
-if n < 0:
-    print("O valor digitado é negativo!")
-else:
-    print("O valor digitado é positivo!")
