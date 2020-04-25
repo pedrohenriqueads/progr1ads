a88064a7752371d5632ee48a33e8a45d907debc6
pedrohenriqueads <pedrosilva@ads.fiponline.edu.br>
2020-04-24 22:04:51 -0300

    atualizando

--- questao4_lista2_conversão_fahrenheit.py
+++ questao4_lista2_conversão_fahrenheit.py
@@ -3,11 +3,11 @@
 
 temp_f = float(input("Digite a temperatura em fahrenheit: "))
 celsius = 5 * (temp_f - 32) / 9
-print("A temperatura em celsius é %.2f graus" % celsius)
+print("A temperatura em celsius é %.f graus" % celsius)
 
 #convertendo para fahrenheit
 
 temp_c = float(input("Digite a temperatura em celsius: "))
 fahrenheit = temp_c * (9 / 5) + 32
-print("A temperatura em fahrenheit é %.2f graus" % fahrenheit)
+print("A temperatura em fahrenheit é %.f graus" % fahrenheit)
 
